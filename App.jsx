c004a4511f912c9bc1b065a54e8e8a21ad036b3e
NetworkPieWeb <88339196+NetworkPieWeb@users.noreply.github.com>
2025-12-01 13:05:51 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,27 +1,3 @@
-import React from 'react';
-import {Routes,Route} from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-
 export default function App(){
-  return (
-    <div className='min-h-screen flex flex-col'>
-      <Header/>
-      <main className='flex-1'>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/portfolio' element={<Portfolio/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-        </Routes>
-      </main>
-      <Footer/>
-    </div>
-  );
+  return <h1 style={{padding:'40px'}}>NetworkPie Vercel Build Works</h1>;
 }
